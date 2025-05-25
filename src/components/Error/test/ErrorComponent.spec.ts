866e9ffb581cfa4baa52538dd62d759de0213382
Lukasz-Lipinski <l.lipinski1993@gmail.com>
2025-05-25 21:29:24 +0200

    error component test fixed

--- src/components/Error/test/ErrorComponent.spec.ts
+++ src/components/Error/test/ErrorComponent.spec.ts
@@ -1,5 +1,5 @@
-import { mount, shallowMount, type VueWrapper } from '@vue/test-utils'
-import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { mount, type VueWrapper } from '@vue/test-utils'
+import { beforeEach, describe, expect, it } from 'vitest'
 import ErrorComponent from '../ErrorComponent.vue'
 import { VAlert, VBtn, VContainer } from 'vuetify/components'
 import { nextTick } from 'vue'
