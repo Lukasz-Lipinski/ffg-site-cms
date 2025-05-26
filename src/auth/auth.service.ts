6028467ee4b8ba9b06f68f516b85c155acb1b3a9
Lukasz-Lipinski <l.lipinski1993@gmail.com>
2025-05-26 21:48:02 +0200

    tests added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,18 @@
-import axios from 'axios'
-import { SnackbarColorType } from '@/stores/snackbar.ts'
-
 import { getAuth } from 'firebase/auth'
 import { firebaseAuth } from '@/auth/auth.config.ts'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth/web-extension'
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth/web-extension'
 
 const auth = getAuth(firebaseAuth)
 
 async function CreateNewAccount(email: string, password: string) {
-  return await createUserWithEmailAndPassword(auth, email, password);
+  return await createUserWithEmailAndPassword(auth, email, password)
 }
 
 async function SignInUser(email: string, password: string) {
   return await signInWithEmailAndPassword(auth, email, password)
 }
 
-export {
-  CreateNewAccount,
-  SignInUser
-}
+export { CreateNewAccount, SignInUser }
---
+++ src/components/LoginForm/types.ts
@@ -0,0 +1,4 @@
+export enum FormOptions {
+  Login = 'Login',
+  Register = 'Register',
+}
